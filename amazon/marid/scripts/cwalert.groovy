638a9347534cf18a9507b259939622900baa90fb
unknown <Administrator@mustafa.(none)>
2012-08-23 23:34:53 +0300

    amazon config moved to marid conf

--- amazon/marid/scripts/cwalert.groovy
+++ amazon/marid/scripts/cwalert.groovy
@@ -3,12 +3,12 @@ recipients = "cloudwatchgroup"
 ATTACH_GRAPHS = true
 def snsMessageConfig = [
     ADDITIONAL_METRICS:[CPUUtilization:["DiskReadBytes", "NetworkIn", "NetworkOut" , "DiskWriteBytes"]],
-    AWS_ACCESS_KEY:"<amazon_access_key>",
-    AWS_SECRET_KEY:"<amazon_secret_key>",
     LAST_N_DAYS:10,
     STAT_PERIOD:300,
 ]
 /*************************************************************/
+snsMessageConfig.AWS_ACCESS_KEY = conf.AWS_ACCESS_KEY
+snsMessageConfig.AWS_SECRET_KEY = conf.AWS_SECRET_KEY
 AmazonSnsMessage snsMessage = new AmazonSnsMessage(request, snsMessageConfig)
 /*********************************************/
 if(AmazonSnsMessage.MESSAGE_TYPE_SUBSCRIPTION_CONFIRMATION.equals(snsMessage.getRequestType())){
