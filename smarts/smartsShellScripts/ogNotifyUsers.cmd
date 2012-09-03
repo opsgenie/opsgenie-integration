d499b8699f4670c1d6f136d58f08636366e84ad5
abdurrahim eke <abdurrahim.eke@ifountain.com>
2012-09-03 15:29:26 +0300

    smarts integration script refactor

--- smarts/smartsShellScripts/ogNotifyUsers.cmd
+++ smarts/smartsShellScripts/ogNotifyUsers.cmd
@@ -10,8 +10,8 @@ SET ACTIONS="acknowledge,unacknowledge,take ownership,release ownership"
 IF "%SM_POBJ_Recipients%"=="" SET RECIPIENTS="web_operations"
 
 echo Creating Alert
-CMD /c %LAMP_HOME%\lamp.bat createAlert --message %MESSAGE% --recipients %RECIPIENTS% --description %DESCRIPTION% ^
---source Smarts --actions %ACTIONS% ^
+CMD /c %LAMP_HOME%\lamp.bat createAlert --message %MESSAGE% --recipients %RECIPIENTS% ^
+--description %DESCRIPTION% --source Smarts --actions %ACTIONS% ^
 -DElementName="%SM_OBJ_ElementName%" -DDomain="%SM_OBJ_SourceDomainName%" -DCount="%SM_OBJ_OccurrenceCount%" ^
 -DNotificationName="%SM_OBJ_Name%" -DDomainName="%SM_SERVER_NAME%"
 
