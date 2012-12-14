b06f436614c79df703d54ff330a727bfeb2060f7
sezgin <sezgin.kucukkaraaslan@ifountain.com>
2012-12-14 13:38:19 +0200

    nagios marid integration

--- nagios/lamp/scripts/ogCreateAlert.groovy
+++ nagios/lamp/scripts/ogCreateAlert.groovy
@@ -18,15 +18,14 @@ import java.text.SimpleDateFormat
 
 // Recipients should be specified here for automatic tools. 
 // Recipients can be users or groups created in OpsGenie
-RECIPIENTS="all"
-SOURCE="Nagios"
-
+RECIPIENTS = "john.smith@ifountain.com"
+SOURCE = "Nagios"
 
 //Nagios credentials are needed for extra information to be fetched through Nagios CGIs
 NAGIOS_USER = "nagiosadmin"
 NAGIOS_PASSWORD = "admin"
-NAGIOS_HOST="localhost"
-NAGIOS_PORT=80
+NAGIOS_HOST = "localhost"
+NAGIOS_PORT = 80
 
 //http client preparation
 def timeout = 30000;
@@ -35,27 +34,37 @@ HttpParams httpClientParams = new BasicHttpParams();
 HttpConnectionParams.setConnectionTimeout(httpClientParams, timeout);
 HttpConnectionParams.setSoTimeout(httpClientParams, timeout);
 HttpConnectionParams.setTcpNoDelay(httpClientParams, true);
-HTTP_CLIENT= new DefaultHttpClient(httpClientParams);
+HTTP_CLIENT = new DefaultHttpClient(httpClientParams);
 AuthScope scope = new AuthScope(TARGET_HOST.getHostName(), TARGET_HOST.getPort());
 HTTP_CLIENT.getCredentialsProvider().setCredentials(scope, new UsernamePasswordCredentials(NAGIOS_USER, NAGIOS_PASSWORD));
 
 def entity = params.entity;
+def action
+def attachFile = true
 
-if(entity == "host" || entity == "service"){
+if (entity == "host" || entity == "service") {
     def alertProps = [:]
-	alertProps.message = params.subject
-	alertProps.recipients = RECIPIENTS
-	alertProps.description = params.textMessage
-	alertProps.source = SOURCE
+    def alias
+
+    alertProps.message = params.subject
+    alertProps.recipients = RECIPIENTS
+    alertProps.description = params.textMessage
+    alertProps.source = SOURCE
     def notificationType = System.getenv('NAGIOS_NOTIFICATIONTYPE')
+    logger.warn("notificationType: ${notificationType}")
+
     def hostAddress = System.getenv('NAGIOS_HOSTADDRESS')
-    def dateTime =  System.getenv('NAGIOS_LONGDATETIME')
-    if(entity == "host"){
+    def dateTime = System.getenv('NAGIOS_LONGDATETIME')
+    if (entity == "host") {
         def hostName = System.getenv('NAGIOS_HOSTNAME')
         def hostState = System.getenv('NAGIOS_HOSTSTATE');
-
-        alertProps.message= "** ${notificationType} Host Alert: ${hostName} is ${hostState} **"
-        alertProps.description = """***** Nagios *****
+        alias = hostName
+        if (hostState == "DOWN") {
+            action = "createAlert"
+            alertProps.alias = alias
+            alertProps.details = ["host": hostName]
+            alertProps.message = "** ${notificationType} Host Alert: ${hostName} is ${hostState} **"
+            alertProps.description = """***** Nagios *****
 
 Notification Type: ${notificationType}
 Host: ${hostName}
@@ -64,13 +73,26 @@ Address: ${hostAddress}
 Additional Info: ${System.getenv('NAGIOS_HOSTOUTPUT')}
 Date/Time: ${dateTime}
 """
+        } else {
+            if (hostState == "UP") {
+                action = "closeAlert"
+            }
+        }
     }
-    else{
+    else {
         def service = System.getenv("NAGIOS_SERVICEDESC")
         def serviceState = System.getenv("NAGIOS_SERVICESTATE");
         def hostAlias = System.getenv("NAGIOS_HOSTALIAS");
-        alertProps.message=  "** ${notificationType} Service Alert: ${hostAlias}/${service} is ${serviceState} **"
-        alertProps.description = """***** Nagios *****
+        def hostName = System.getenv("NAGIOS_HOSTNAME");
+        alias = hostName + "_" + service
+        logger.warn("service state: ${serviceState}")
+
+        if (serviceState == "CRITICAL") {
+            action = "createAlert"
+            alertProps.alias = alias
+            alertProps.details = ["host": hostName, "service": service]
+            alertProps.message = "** ${notificationType} Service Alert: ${hostAlias}/${service} is ${serviceState} **"
+            alertProps.description = """***** Nagios *****
 
 Notification Type: ${notificationType}
 Service: ${service}
@@ -80,60 +102,71 @@ State: ${serviceState}
 Additional Info: ${System.getenv('NAGIOS_SERVICEOUTPUT')}
 Date/Time: ${dateTime}
 """
+        } else {
+            if (serviceState == "OK") {
+                action = "closeAlert"
+            }
+        }
     }
-	logger.warn("Creating alert with message ${alertProps.message}");
-	println "Creating alert"
-	def response = opsgenie.createAlert(alertProps)
-	def alertId =  response.alertId;
-	logger.warn("Alert is created with id :"+alertId);
-	println "Alert is created with id :"+alertId
-	attach(alertId, entity)
-}
-else{
-	logger.warn("Invalid notification mode ${entity}")
-	println "Invalid notification mode ${entity}"
+    if (action == "createAlert") {
+        logger.warn("Creating alert with message ${alertProps.message}");
+        println "Creating alert"
+        def response = opsgenie.createAlert(alertProps)
+        def alertId = response.alertId;
+        logger.warn("Alert is created with id :" + alertId);
+        println "Alert is created with id :" + alertId
+        if (attachFile) { attach(alertId, entity) }
+    } else {
+        if (action == "closeAlert") {
+            logger.warn("Closing the alert ${alias}")
+            def response = opsgenie.closeAlert([alias: alias])
+        }
+    }
+} else {
+    logger.warn("Invalid notification mode ${entity}")
+    println "Invalid notification mode ${entity}"
 }
 
-def attach(alertId, entity){
+def attach(alertId, entity) {
     // will get alert histgoram and trends charts from nagios and attach them to the alert
-    try{
+    try {
         def alertHistogram = getAlertHistogram(entity);
         def trends = getTrends(entity);
         def htmlText = createHtml(entity, alertHistogram, trends)
         ByteArrayOutputStream bout = createZip(htmlText, alertHistogram, trends)
         logger.warn("Attaching details");
         println "Attaching details"
-        response = opsgenie.attach([alertId:alertId, stream:new ByteArrayInputStream(bout.toByteArray()), fileName:"details.zip"])
-        if(response.success){
+        response = opsgenie.attach([alertId: alertId, stream: new ByteArrayInputStream(bout.toByteArray()), fileName: "details.zip"])
+        if (response.success) {
             logger.warn("Successfully attached details");
             println "Successfully attached details"
         }
-        else{
+        else {
             println "Could not attach details"
             logger.warn("Could not attach details");
         }
     }
-    catch(e){
+    catch (e) {
         logger.warn("Could not attach details. Reason: ${e.getMessage()}")
         println "Could not attach details. Reason: ${e.getMessage()}"
     }
 }
 
-def createZip(html, alertHistogram, trends){
+def createZip(html, alertHistogram, trends) {
     ByteArrayOutputStream bout = new ByteArrayOutputStream();
     ZipOutputStream zout = new ZipOutputStream(bout);
     ZipEntry htmlEntry = new ZipEntry("index.html");
     zout.putNextEntry(htmlEntry);
     zout.write(html.getBytes())
     zout.closeEntry()
-    if(alertHistogram){
+    if (alertHistogram) {
         ZipEntry imageEntry = new ZipEntry("alertHistogram.png");
         zout.putNextEntry(imageEntry);
         BufferedImage img = ImageIO.read(new ByteArrayInputStream(alertHistogram))
         ImageIO.write(img, "png", zout);
         zout.closeEntry()
     }
-    if(trends){
+    if (trends) {
         ZipEntry imageEntry = new ZipEntry("trends.png");
         zout.putNextEntry(imageEntry);
         BufferedImage img = ImageIO.read(new ByteArrayInputStream(trends))
@@ -144,7 +177,7 @@ def createZip(html, alertHistogram, trends){
     return bout;
 }
 
-def createHtml(entity, alertHistogram, trends){
+def createHtml(entity, alertHistogram, trends) {
     StringBuffer buf = new StringBuffer()
     buf.append("""
         <html>
@@ -161,16 +194,16 @@ def createHtml(entity, alertHistogram, trends){
             <body>
                 <div>
     """)
-    if(entity == "host"){
+    if (entity == "host") {
         getHostStatusHtml(buf)
     }
-    else{
+    else {
         getServicesStatusHtml(buf)
     }
-    if(trends){
+    if (trends) {
         buf.append('<div class="img"><img src="trends.png"></div>')
     }
-    if(alertHistogram){
+    if (alertHistogram) {
         buf.append('<div class="img"><img src="alertHistogram.png"></div>')
     }
     buf.append("""
@@ -181,16 +214,16 @@ def createHtml(entity, alertHistogram, trends){
     return buf.toString();
 }
 
-def getServicesStatusHtml(buf){
-   SimpleDateFormat dateFormatter = new SimpleDateFormat("MM-dd-yyyy hh:mm:ss")
-   def serviceGroup = System.getenv('NAGIOS_SERVICEGROUPNAME');
-   def memberOf = serviceGroup ? serviceGroup : 'No service groups'
-   def state = System.getenv('NAGIOS_SERVICESTATE');
-   def lastServiceCheck = System.getenv("NAGIOS_LASTSERVICECHECK");
-   def lastStateChange = System.getenv("NAGIOS_LASTSERVICESTATECHANGE");
-   lastServiceCheck = dateFormatter.format(Long.parseLong(lastServiceCheck) * 1000L)
-   lastStateChange = dateFormatter.format(Long.parseLong(lastStateChange) * 1000L)
-   buf.append("""
+def getServicesStatusHtml(buf) {
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("MM-dd-yyyy hh:mm:ss")
+    def serviceGroup = System.getenv('NAGIOS_SERVICEGROUPNAME');
+    def memberOf = serviceGroup ? serviceGroup : 'No service groups'
+    def state = System.getenv('NAGIOS_SERVICESTATE');
+    def lastServiceCheck = System.getenv("NAGIOS_LASTSERVICECHECK");
+    def lastStateChange = System.getenv("NAGIOS_LASTSERVICESTATECHANGE");
+    lastServiceCheck = dateFormatter.format(Long.parseLong(lastServiceCheck) * 1000L)
+    lastStateChange = dateFormatter.format(Long.parseLong(lastStateChange) * 1000L)
+    buf.append("""
         <div class="well">
             <table>
                 <tbody>
@@ -211,7 +244,8 @@ def getServicesStatusHtml(buf){
         </div>
    """)
 }
-def getHostStatusHtml(buf){
+
+def getHostStatusHtml(buf) {
     SimpleDateFormat dateFormatter = new SimpleDateFormat("MM-dd-yyyy hh:mm:ss")
     def hostGroup = System.getenv('NAGIOS_HOSTGROUPNAME');
     def memberOf = hostGroup ? hostGroup : 'No host groups'
@@ -241,35 +275,36 @@ def getHostStatusHtml(buf){
    """)
 }
 
-def getAlertHistogram(entity){
+def getAlertHistogram(entity) {
     return getImage("/nagios/cgi-bin/histogram.cgi", entity);
 }
 
-def getTrends(entity){
+def getTrends(entity) {
     return getImage("/nagios/cgi-bin/trends.cgi", entity);
 }
 
-def getImage(url, entity){
+def getImage(url, entity) {
     def host = System.getenv("NAGIOS_HOSTNAME")
     url += "?createimage&host=" + URLEncoder.encode(host)
-    if(entity == "service"){
+    if (entity == "service") {
         def service = System.getenv("NAGIOS_SERVICEDESC")
-        url+="&service=" + URLEncoder.encode(service)
+        url += "&service=" + URLEncoder.encode(service)
     }
     HttpGet httpGet = new HttpGet(url);
     def response = HTTP_CLIENT.execute(TARGET_HOST, httpGet);
-    if(response.getStatusLine().getStatusCode() == 200){
+    if (response.getStatusLine().getStatusCode() == 200) {
         logger.warn("Image received");
         println "Image received"
         return EntityUtils.toByteArray(response.getEntity());
     }
-    else{
+    else {
         logger.warn("Could not get image from url ${url}")
         println "Could not get image from url ${url}"
         return null;
     }
 }
 
-def htmlEscape(value){
+def htmlEscape(value) {
     return StringEscapeUtils.escapeHtml(value)
 }
+
---
+++ nagios/marid/scripts/nagiosActionExecutor.groovy
@@ -0,0 +1,83 @@
+import org.apache.http.HttpHost
+import org.apache.http.params.HttpParams
+import org.apache.http.params.BasicHttpParams
+import org.apache.http.params.HttpConnectionParams
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.client.methods.HttpPost
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.message.BasicNameValuePair
+import org.apache.http.util.EntityUtils
+
+LOG_PREFIX = "[${action}]:";
+logger.warn("${LOG_PREFIX} Will execute action for alertId ${alert.alertId}");
+
+//http client preparation
+def timeout = conf["nagios.http.timeout"].toInteger();
+TARGET_HOST = new HttpHost(conf["nagios.host"], conf["nagios.port"].toInteger(), "http");
+HttpParams httpClientParams = new BasicHttpParams();
+HttpConnectionParams.setConnectionTimeout(httpClientParams, timeout);
+HttpConnectionParams.setSoTimeout(httpClientParams, timeout);
+HttpConnectionParams.setTcpNoDelay(httpClientParams, true);
+HTTP_CLIENT = new DefaultHttpClient(httpClientParams);
+AuthScope scope = new AuthScope(TARGET_HOST.getHostName(), TARGET_HOST.getPort());
+HTTP_CLIENT.getCredentialsProvider().setCredentials(scope, new UsernamePasswordCredentials(conf["nagios.user"], conf["nagios.password"]));
+
+
+try {
+    def alert = opsgenie.getAlert(alertId: alert.alertId)
+    if (alert.size() > 0) {
+        def host = alert.details.host
+        def service = alert.details.service
+        def postParams = ["btnSubmit": "Commit", "persistent": "on", "cmd_mod": "2", "send_notification": "off", "host": host]
+        if(service) postParams.service = service;
+        if(action == "Acknowledge"){
+            postParams.com_data = "Acknowledged by ${alert.username} via OpsGenie"
+            postParams.sticky_ack = "on"
+            postParams.cmd_typ = service ? "34" : "33";
+        }
+        else if(action == "TakeOwnership"){
+            postParams.com_data = "alert ownership taken by ${alert.username}"
+            postParams.cmd_typ = service ? "3" : "1";
+        }
+        else if(action == "Assign"){
+            postParams.com_data = "alert ownership assigned to ${alert.owner}"
+            postParams.cmd_typ = service ? "3" : "1";
+        }
+        else if(action == "AddNote"){
+            postParams.com_data = "${alert.note} by ${alert.username}"
+            postParams.cmd_typ = service ? "3" : "1";
+        }
+        postToNagios(postParams)
+    }
+    else {
+        logger.warn("${LOG_PREFIX} Alert with id [${alert.alertId}] does not exist in OpsGenie. It is probably deleted.")
+    }
+}
+finally {
+    HTTP_CLIENT.getConnectionManager().shutdown();
+}
+
+
+
+def postToNagios(postParams){
+    HttpPost post = new HttpPost("/nagios/cgi-bin/cmd.cgi")
+    def formparams = [];
+    postParams.each{key, value ->
+        formparams << new BasicNameValuePair(key, value)
+    }
+    UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+    post.setEntity(entity);
+    def response = HTTP_CLIENT.execute(TARGET_HOST, post);
+    if(response.getStatusLine().getStatusCode() == 200){
+        logger.info("${LOG_PREFIX} Successfully executed at Nagios.");
+        EntityUtils.consume(response.getEntity());
+    }
+    else{
+        logger.warn("${LOG_PREFIX} Could not execute at Nagios.")
+    }
+}
+
+
+
