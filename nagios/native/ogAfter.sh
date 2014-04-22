b83b3600790eefe210b50c6c881b4766304b7652
mustafa <mustafa.sener@ifountain.com>
2014-04-22 14:41:22 +0300

    client package fix

--- nagios/native/ogAfter.sh
+++ nagios/native/ogAfter.sh
@@ -1,2 +1,5 @@
-chmod 755 /usr/local/bin/opsgenie
+chmod 755 /usr/local/bin/nagios2opsgenie
 usermod -a -G opsgenie nagios
+if [ ! -d "/usr/local/nagios/etc/objects" ]; then
+    cp /etc/opsgenie/opsgenie.cfg  /usr/local/nagios/etc/objects/
+fi
