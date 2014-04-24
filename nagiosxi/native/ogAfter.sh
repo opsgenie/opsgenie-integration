46e47cdf1a1d68d367815d521ebc174be6e0f1a4
mustafa <mustafa.sener@ifountain.com>
2014-04-24 17:39:50 +0300

    client packaging fixes

--- nagiosxi/native/ogAfter.sh
+++ nagiosxi/native/ogAfter.sh
@@ -2,5 +2,5 @@ chmod 755 /usr/local/bin/nagios2opsgenie
 if id -u nagios >/dev/null 2>&1; then
         usermod -a -G opsgenie nagios
 else
-        echo "nagios user does not exist. Please don't forget to add your nagios user to opsgenie group!"
+        echo "-   nagios user does not exist. Please don't forget to add your nagios user to opsgenie group!"
 fi
