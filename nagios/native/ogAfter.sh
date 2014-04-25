6f78625a0a73c7b8a146c9fbee0f2b3a8c6ca106
mustafa <mustafa.sener@ifountain.com>
2014-04-25 09:29:27 +0300

    lamp sh fixes

--- nagios/native/ogAfter.sh
+++ nagios/native/ogAfter.sh
@@ -2,11 +2,11 @@ chmod 755 /usr/local/bin/nagios2opsgenie
 if id -u nagios >/dev/null 2>&1; then
         usermod -a -G opsgenie nagios
 else
-        echo "-   nagios user does not exist. Please don't forget to add your nagios user to opsgenie group!"
+        echo "WARNING : nagios user does not exist. Please don't forget to add your nagios user to opsgenie group!"
 fi
 
 if [ -d "/usr/local/nagios/etc/objects" ]; then
     cp /etc/opsgenie/opsgenie.cfg  /usr/local/nagios/etc/objects/
 else
-        echo "-   Could not find your NAGIOS_HOME directory. Please copy /etc/opsgenie/opsgenie.cfg file to your <NAGIOS_HOME>/etc/objects directory manually!"
+        echo "WARNING : Could not find your NAGIOS_HOME directory. Please copy /etc/opsgenie/opsgenie.cfg file to your <NAGIOS_HOME>/etc/objects directory manually!"
 fi
