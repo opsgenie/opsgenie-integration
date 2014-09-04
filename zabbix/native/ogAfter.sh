5cdead83d10fc54517988eea7df951e8db41fe9e
halit <halit.okumus@ifountain.com>
2014-09-04 11:28:11 +0300

    try newgrp command

--- zabbix/native/ogAfter.sh
+++ zabbix/native/ogAfter.sh
@@ -2,6 +2,7 @@ chmod 755 /etc/opsgenie/zabbix2opsgenie
 
 if id -u zabbix >/dev/null 2>&1; then
         usermod -a -G opsgenie zabbix
+        newgrp opsgenie
 else
         echo "WARNING : zabbix user does not exist. Please don't forget to add your zabbix user to opsgenie group!"
 fi
