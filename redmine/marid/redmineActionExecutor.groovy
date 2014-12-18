fc416f0f69cf1f2de6ff7da4abbd9d312dd61267
halit <halit.okumus@ifountain.com>
2014-12-18 09:18:13 +0200

    redmine integration

---
+++ redmine/marid/redmineActionExecutor.groovy
@@ -0,0 +1,127 @@
+import com.ifountain.opsgenie.client.http.OpsGenieHttpClient
+import com.ifountain.opsgenie.client.util.ClientConfiguration
+import com.ifountain.opsgenie.client.util.JsonUtils
+import org.apache.http.entity.StringEntity
+
+LOG_PREFIX = "[${action}]:";
+logger.warn("${LOG_PREFIX} Will execute action for alertId ${alert.alertId}");
+
+CONF_PREFIX = "redmine.";
+alertFromOpsgenie = opsgenie.getAlert(alertId: alert.alertId)
+HTTP_CLIENT = createHttpClient();
+try{
+    if (alertFromOpsgenie.size() > 0) {
+        if (action == 'Create'){
+            def contentParams = [:]
+            def issueMap = [:]
+            issueMap.project_id = Integer.parseInt(_conf("projectId", true) as String)
+            issueMap.subject = "New Alert : " +  alertFromOpsgenie.message
+            issueMap.description = alertFromOpsgenie.description
+
+            def customFieldId = _conf("customFieldId", false) as String
+            if(customFieldId) {
+                def customFields = []
+                customFields.add([
+                        "id"   : Integer.parseInt(customFieldId),
+                        "value": alertFromOpsgenie.alertId
+                ])
+                issueMap.put("custom_fields", customFields)
+            }
+
+            contentParams.put("issue", issueMap)
+            String content = JsonUtils.toJson(contentParams)
+
+            String url = _conf("url", true) + "/issues.json"
+
+            def requestParameters = [:]
+            requestParameters.key = _conf("apiKey", true)
+
+            logger.debug("${LOG_PREFIX} Posting to Redmine. Url ${url} params:${requestParameters} content:${content}")
+            def post = ((OpsGenieHttpClient) HTTP_CLIENT).preparePostMethod(url, content, ["Content-Type":"application/json"], requestParameters)
+            ((StringEntity)post.getEntity()).setChunked(false)
+            def response = ((OpsGenieHttpClient) HTTP_CLIENT).executeHttpMethod(post)
+
+            if (response.getStatusCode() < 299) {
+                logger.info("${LOG_PREFIX} Successfully executed at Redmine.");
+                logger.debug("${LOG_PREFIX} Redmine response: ${response.statusCode} ${response.getContentAsString()}")
+            } else {
+                logger.warn("${LOG_PREFIX} Could not execute at Redmine; response: ${response.statusCode} ${response.getContentAsString()}")
+            }
+        }
+        else if(action == "AddNote"){
+            def customFieldId = _conf("customFieldId", false) as String
+            if(customFieldId) {
+                String url = _conf("url", true) + "/issues.json"
+
+                def requestParameters = [:]
+                requestParameters.key = _conf("apiKey", true)
+                requestParameters.put("cf_${customFieldId}".toString(), alertFromOpsgenie.alertId)
+
+                logger.debug("${LOG_PREFIX} executing GET request. Url ${url} params:${requestParameters}")
+                def response = ((OpsGenieHttpClient) HTTP_CLIENT).get(url, requestParameters)
+                if (response.getStatusCode() < 299) {
+                    logger.info("${LOG_PREFIX} Successfully executed at Redmine.");
+                    logger.debug("${LOG_PREFIX} Redmine response: ${response.statusCode} ${response.getContentAsString()}")
+
+                    def responseMap = JsonUtils.parse(response.contentAsString)
+                    if(responseMap.total_count > 0){
+                        def issueId = responseMap.issues[0].id
+                        url = _conf("url", true) + "/issues/${issueId}.json"
+
+                        requestParameters = [:]
+                        requestParameters.key = _conf("apiKey", true)
+
+                        def contentParams = [:]
+                        def issueMap = [:]
+                        issueMap.notes = alert.note + " by " + alert.username
+                        contentParams.put("issue", issueMap)
+
+                        def put = ((OpsGenieHttpClient) HTTP_CLIENT).preparePutMethod(url, contentParams, requestParameters)
+                        ((StringEntity)put.getEntity()).setChunked(false)
+                        put.setHeader("Content-Type", "application/json")
+                        response = ((OpsGenieHttpClient) HTTP_CLIENT).executeHttpMethod(put)
+                        if (response.getStatusCode() < 299) {
+                            logger.info("${LOG_PREFIX} Successfully executed at Redmine.");
+                            logger.debug("${LOG_PREFIX} Redmine response: ${response.statusCode} ${response.getContentAsString()}")
+                        } else {
+                            logger.warn("${LOG_PREFIX} Could not execute at Redmine; response: ${response.statusCode} ${response.getContentAsString()}")
+                        }
+                    } else{
+                        logger.warn("${LOG_PREFIX} No issue could be found")
+                    }
+                } else {
+                    logger.warn("${LOG_PREFIX} Could not execute at Redmine; response: ${response.statusCode} ${response.getContentAsString()}")
+                }
+            }
+        }
+    }
+    else {
+        logger.warn("${LOG_PREFIX} Alert with id [${alert.alertId}] does not exist in OpsGenie. It is probably deleted.")
+    }
+}
+finally {
+    HTTP_CLIENT.close()
+}
+
+def createHttpClient() {
+    def timeout = _conf("http.timeout", false);
+    if(timeout == null){
+        timeout = 30000;
+    }
+    else{
+        timeout = timeout.toInteger();
+    }
+
+    ClientConfiguration clientConfiguration = new ClientConfiguration().setSocketTimeout(timeout)
+    return new OpsGenieHttpClient(clientConfiguration)
+}
+def _conf(confKey, boolean isMandatory)
+{
+    def confVal = conf[CONF_PREFIX+confKey]
+    if(isMandatory && confVal == null){
+        def errorMessage = "${LOG_PREFIX} Skipping action, Mandatory Conf item ${CONF_PREFIX+confKey} is missing. Check your marid conf file.";
+        throw new Exception(errorMessage);
+    }
+    return confVal
+}
+
