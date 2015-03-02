193fa2c67a8ede6e8832b95c890e2d93135e1a8a
Ömer Özkan <omer@ozkan.info>
2015-03-02 13:32:35 +0200

    udpateGit task added

---
+++ updateGit.sh
@@ -0,0 +1,21 @@
+# /bin/bash
+
+INTEGRATION_PATH=$1
+CLIENT_PATH=$2
+COMMIT_MSG=$3
+TAG_NAME=$4
+
+cd $INTEGRATION_PATH
+/usr/bin/git checkout master
+/usr/bin/git add version.properties
+/usr/bin/git commit -m "$COMMIT_MSG"
+
+/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
+
+/usr/bin/git push origin master
+/usr/bin/git push --tags
+
+cd $CLIENT_PATH
+/usr/bin/git checkout master
+/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
+/usr/bin/git push --tags
