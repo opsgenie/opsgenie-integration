d051cbc6a93d89405113520d80aa884afe3b2a03
tubaozturk <tuba.ozturk@ifountain.com>
2015-08-05 17:29:26 +0300

    deprecated integrations removal

--- smarts/smartsShellScripts/ogNotifyUsersRich.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Specify the location of the lamp utility
-LAMP_HOME=/opt/lamp
-
-$LAMP_HOME/lamp executeScript --name ogNotifyUsers.groovy
--- splunk/splunkActionScripts/s2og.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-::####################################CONFIGURATION_SETTIGNS###############################
-SET LAMP_HOME=C:\lamp
-::#########################################################################################
-
-
-CALL "%LAMP_PATH%\lamp" executeScript --name createAlertWithResults.groovy >> "%SPLUNK_HOME%/bin/scripts/opsgenie_output.txt"
