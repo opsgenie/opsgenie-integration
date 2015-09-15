81c94742b43c2df38d951dae57fa222a0f9fb823
Ömer Özkan <omer@ozkan.info>
2015-09-15 12:04:12 +0300

    updateGit enabled

--- updateGit.sh
+++ updateGit.sh
@@ -5,23 +5,21 @@ CLIENT_PATH=$2
 COMMIT_MSG=$3
 TAG_NAME=$4
 
-exit 1
+cd $INTEGRATION_PATH
+/usr/bin/git stash save before_release
+/usr/bin/git checkout master
+/usr/bin/git pull origin master
+/usr/bin/git stash pop
+/usr/bin/git add version.properties
+/usr/bin/git commit -m "$COMMIT_MSG"
 
-#cd $INTEGRATION_PATH
-#/usr/bin/git stash save before_release
-#/usr/bin/git checkout master
-#/usr/bin/git pull origin master
-#/usr/bin/git stash pop
-#/usr/bin/git add version.properties
-#/usr/bin/git commit -m "$COMMIT_MSG"
-#
-#/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
-#
-#/usr/bin/git push origin master
-#/usr/bin/git push --tags
-#
-#
-#cd $CLIENT_PATH
-#/usr/bin/git checkout master
-#/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
-#/usr/bin/git push --tags
+/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
+
+/usr/bin/git push origin master
+/usr/bin/git push --tags
+
+
+cd $CLIENT_PATH
+/usr/bin/git checkout master
+/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
+/usr/bin/git push --tags
