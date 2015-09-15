5fccd0234cc913085c60b0d09250e625535b4f8e
Ömer Özkan <omer@ozkan.info>
2015-09-15 11:06:53 +0300

    updateGit disabled temporary for test

--- updateGit.sh
+++ updateGit.sh
@@ -5,21 +5,23 @@ CLIENT_PATH=$2
 COMMIT_MSG=$3
 TAG_NAME=$4
 
-cd $INTEGRATION_PATH
-/usr/bin/git stash save before_release
-/usr/bin/git checkout master
-/usr/bin/git pull origin master
-/usr/bin/git stash pop
-/usr/bin/git add version.properties
-/usr/bin/git commit -m "$COMMIT_MSG"
+exit 1
 
-/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
-
-/usr/bin/git push origin master
-/usr/bin/git push --tags
-
-
-cd $CLIENT_PATH
-/usr/bin/git checkout master
-/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
-/usr/bin/git push --tags
+#cd $INTEGRATION_PATH
+#/usr/bin/git stash save before_release
+#/usr/bin/git checkout master
+#/usr/bin/git pull origin master
+#/usr/bin/git stash pop
+#/usr/bin/git add version.properties
+#/usr/bin/git commit -m "$COMMIT_MSG"
+#
+#/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
+#
+#/usr/bin/git push origin master
+#/usr/bin/git push --tags
+#
+#
+#cd $CLIENT_PATH
+#/usr/bin/git checkout master
+#/usr/bin/git tag -a $TAG_NAME -m $TAG_NAME
+#/usr/bin/git push --tags
