0fccd7c44f056b915c4866e7a798a4ad0c82eba5
Ufuk <ufuk@opsgenie.com>
2017-07-31 19:48:00 +0300

    ICS-1283 change ogAfter of OP5

--- op5/native/ogAfter.sh
+++ op5/native/ogAfter.sh
@@ -17,7 +17,11 @@ if [ -d "/opt/monitor/etc/mconf" ]; then
     cp /etc/opsgenie/opsgenie.cfg  /opt/monitor/etc/mconf
     cp /etc/opsgenie/opsgenie.cfg  /opt/monitor/etc
     chown monitor:apache /etc/opsgenie/opsgenie.cfg
+    chown monitor:apache /opt/monitor/etc/opsgenie.cfg
+    chown monitor:apache /opt/monitor/etc/mconf/opsgenie.cfg
     chmod 664 /etc/opsgenie/opsgenie.cfg
+    chmod 664 /opt/monitor/etc/opsgenie.cfg
+    chmod 664 /opt/monitor/etc/mconf/opsgenie.cfg
 else
         echo "WARNING : Could not find your /opt/monitor directory. Please copy /etc/opsgenie/opsgenie.cfg file to your /opt/monitor/etc/mconf directory manually!"
 fi
